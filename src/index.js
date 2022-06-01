8895d7b657d5bffe95b12101cac9ff5788110906
nhathang99 <17520437@gm.uit.edu.vn>
2022-06-02 00:40:25 +0700

    update ci

--- src/index.js
+++ src/index.js
@@ -1,13 +1,11 @@
 import React from 'react'
-import dotenv from 'dotenv'
+import store from './store'
 import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
 import reportWebVitals from './reportWebVitals'
 import { BrowserRouter } from 'react-router-dom'
 import { Provider } from 'react-redux'
-import store from './store'
-dotenv.config()
 
 ReactDOM.render(
   <React.StrictMode>
